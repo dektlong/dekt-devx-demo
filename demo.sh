7e5b82131881850252685031dbe3479b248dc2e4
dekt <dekel@vmware.com>
2021-11-11 11:42:19 -0800

    done backend inner-loop

--- demo.sh
+++ demo.sh
@@ -16,7 +16,7 @@ create-backend() {
     touch .config/dummy-commit.me
     git add .
     git commit -q -a -m "done backend inner-loop"
-    git push -q
+    git push 
 
     echo
     echo "=========> 2. Apply development routes, mapping and micro-gateway"
