acd38ce582174e8b858040763a8226f0ca35172d
dekt <dekel@vmware.com>
2022-04-13 15:10:21 -0700

    upgrade cluster essentials to 1.1

--- builder.sh
+++ builder.sh
@@ -113,7 +113,7 @@
     #install-tap-prereq
     install-tap-prereq () {
 
-        export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:82dfaf70656b54dcba0d4def85ccae1578ff27054e7533d08320244af7fb0343
+        export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
         export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
         export INSTALL_REGISTRY_USERNAME=$TANZU_NETWORK_USER
         export INSTALL_REGISTRY_PASSWORD=$TANZU_NETWORK_PASSWORD
--- tanzu-cluster-essentials/install.sh
+++ tanzu-cluster-essentials/install.sh
@@ -4,13 +4,20 @@ set -e -o pipefail
 
 # Note script should be idempotent
 
-if command -v xattr &>/dev/null; then
-	xattr -d com.apple.quarantine imgpkg kapp kbld ytt 1>/dev/null 2>&1 || true
+if [ "$(uname)" == "Darwin" ]; then
+	if command -v xattr &>/dev/null; then
+		xattr -d com.apple.quarantine imgpkg kapp kbld ytt 1>/dev/null 2>&1 || true
+	fi
 fi
 
 ns_name=tanzu-cluster-essentials
 echo "## Creating namespace $ns_name"
-kubectl create ns $ns_name 2>/dev/null || true
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: ${ns_name}
+EOF
 
 echo "## Pulling bundle from $INSTALL_REGISTRY_HOSTNAME (username: $INSTALL_REGISTRY_USERNAME)"
 
@@ -29,11 +36,7 @@ export YTT_registry__username=$INSTALL_REGISTRY_USERNAME
 export YTT_registry__password=$INSTALL_REGISTRY_PASSWORD
 
 echo "## Deploying kapp-controller"
-./ytt -f ./bundle/kapp-controller/config/ -f ./bundle/registry-creds/ --data-values-env YTT --data-value-yaml kappController.deployment.concurrency=10 \
-	| ./kbld -f- -f ./bundle/.imgpkg/images.yml \
-	| ./kapp deploy -a kapp-controller -n $ns_name -f- --yes
+./kapp deploy -a kapp-controller -n $ns_name -f <(./ytt -f ./bundle/kapp-controller/config/ -f ./bundle/registry-creds/ --data-values-env YTT --data-value-yaml kappController.deployment.concurrency=10 | ./kbld -f- -f ./bundle/.imgpkg/images.yml) "$@"
 
 echo "## Deploying secretgen-controller"
-./ytt -f ./bundle/secretgen-controller/config/ -f ./bundle/registry-creds/ --data-values-env YTT \
-	| ./kbld -f- -f ./bundle/.imgpkg/images.yml \
-	| ./kapp deploy -a secretgen-controller -n $ns_name -f- --yes
+./kapp deploy -a secretgen-controller -n $ns_name -f <(./ytt -f ./bundle/secretgen-controller/config/ -f ./bundle/registry-creds-schema/ -f ./bundle/registry-creds/ --data-values-env YTT | ./kbld -f- -f ./bundle/.imgpkg/images.yml) "$@"
