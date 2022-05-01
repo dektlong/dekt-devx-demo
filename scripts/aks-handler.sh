91bf04ea60ae6c616cf239a1fbd17d62dc193ee6
dekt <dekel@vmware.com>
2022-05-01 08:14:50 -0700

    add install-carvel script

--- scripts/aks-handler.sh
+++ scripts/aks-handler.sh
@@ -34,18 +34,6 @@ create-aks-cluster() {
 
 }
 
-#add-carvel-tools
-add-carvel-tools () {
-
-	export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
-    export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
-    export INSTALL_REGISTRY_USERNAME=$TANZU_NETWORK_USER
-    export INSTALL_REGISTRY_PASSWORD=$TANZU_NETWORK_PASSWORD
-    pushd scripts/carvel
-        ./install.sh --yes
-	pushd
-}
-
 #delete-aks-cluster
 delete-aks-cluster() {
 
@@ -75,7 +63,7 @@ incorrect-usage() {
 case $1 in
 create)
   	create-aks-cluster $2 $3
-    add-carvel-tools
+    scripts/install-carvel.sh
     ;;
 delete)
     delete-aks-cluster $2
--- scripts/eks-handler.sh
+++ scripts/eks-handler.sh
@@ -35,17 +35,6 @@ create-eks-cluster () {
 }
 
 
-#add-carvel-tools
-add-carvel-tools () {
-
-	export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
-    export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
-    export INSTALL_REGISTRY_USERNAME=$TANZU_NETWORK_USER
-    export INSTALL_REGISTRY_PASSWORD=$TANZU_NETWORK_PASSWORD
-    pushd scripts/carvel
-        ./install.sh --yes
-	pushd
-}
 #delete-cluster
 delete-eks-cluster () {
 
@@ -75,7 +64,7 @@ incorrect-usage() {
 case $1 in
 create)
   	create-eks-cluster $2 $3
-    add-carvel-tools
+    scripts/install-carvel.sh
     ;;
 delete)
     delete-eks-cluster $2
