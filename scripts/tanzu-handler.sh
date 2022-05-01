01ad2fcd5e6f742a74f85efc6a3c6725e9c89289
dekt <dekel@vmware.com>
2022-05-01 08:30:30 -0700

    add tanzu-handler script

--- scripts/tanzu-handler.sh
+++ scripts/tanzu-handler.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-TANZU_NETWORK_USER=$(yq .buildservice.tanzunet_username .config/tap-values-full.yaml)
-TANZU_NETWORK_PASSWORD=$(yq .buildservice.tanzunet_password .config/tap-values-full.yaml)
-
+export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
+export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
+export INSTALL_REGISTRY_USERNAME=$(yq .buildservice.tanzunet_username .config/tap-values-full.yaml)
+export INSTALL_REGISTRY_PASSWORD=$(yq .buildservice.tanzunet_password .config/tap-values-full.yaml)
 
 #add-carvel
 add-carvel () {
-
-    export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
-    export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
-    export INSTALL_REGISTRY_USERNAME=$TANZU_NETWORK_USER
-    export INSTALL_REGISTRY_PASSWORD=$TANZU_NETWORK_PASSWORD
 
     pushd scripts/carvel
 
@@ -21,11 +17,6 @@ add-carvel () {
 
 #remove-carvel
 remove-carvel () {
-
-    export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
-    export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
-    export INSTALL_REGISTRY_USERNAME=$TANZU_NETWORK_USER
-    export INSTALL_REGISTRY_PASSWORD=$TANZU_NETWORK_PASSWORD
 
     pushd scripts/carvel
 
