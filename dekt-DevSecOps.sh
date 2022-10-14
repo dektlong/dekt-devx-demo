af79f6473a6df9cc25ee805e2ee1ad181f6db080
dekt <dekel@vmware.com>
2022-10-14 12:59:03 -0700

    refactor to auto setup of congig yaml via ytt

--- dekt-DevSecOps.sh
+++ dekt-DevSecOps.sh
@@ -4,9 +4,9 @@
 
     #clusters
     VIEW_CLUSTER=$(yq .clusters.view.name .config/demo-values.yaml)
-    DEV_CLUSTER=$(yq .dev-cluster.name .config/demo-values.yaml)
+    DEV_CLUSTER=$(yq .clusters.dev.name .config/demo-values.yaml)
     STAGE_CLUSTER=$(yq .clusters.stage.name .config/demo-values.yaml)
-    PROD_CLUSTER=$(yq .prod-cluster.name .config/demo-values.yaml)
+    PROD_CLUSTER=$(yq .clusters.prod.name .config/demo-values.yaml)
     BROWNFIELD_CLUSTER=$(yq .clusters.brownfield.name .config/demo-values.yaml)
     #workloads (must match the info in .config/workloads)
     PORTAL_WORKLOAD="mood-portal"
