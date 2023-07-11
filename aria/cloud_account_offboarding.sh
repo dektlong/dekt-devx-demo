a215d39c94cc04f6906edaa501f39f2eff3bb0b8
dekel tankel <dekel@vmware.com>
2023-07-10 17:15:28 -0700

    minor

--- aria/cloud_account_offboarding.sh
+++ aria/cloud_account_offboarding.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#
-# Copyright (c) 2022-2023 VMware Inc.
+
+# Copyright (c) 2019-2020 VMware Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,177 +13,282 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 ###################################################################
-# Script Name	  : vss_azure_bulk_event_stream_deactivate.sh
-# Description	  : The script uses az cli and for each subscriptions 1) Delete exist CHSS cc_az_webhook_resource_group resource group 2)  Send stream not ready hookup event to CHSS.
-#                 CSP_REFRESH_TOKEN https://docs.vmware.com/en/CloudHealth-Secure-State/services/api-reference/GUID-getting-started.html is used for authentication
-# Usage         : ./vss_azure_bulk_event_stream_deactivate.sh <Subscription_1> <Subscription_2>
+# Script Name	  : vss_aws_bulk_deactivate_event_stream.sh
+# Description	  : The script uses administrator IAM user with AdministratorAccess permissions https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_accounts_access.html#orgs_manage_accounts_access-cross-account-role
+# Usage         : ./vss_aws_bulk_deactivate_event_stream.sh 857350542564 739636793709 905467601150,OrganizationAccountAccessRole_3
 # Author       	: vss lifters
 # Email         : support@cloudhealthtech.com
 ###################################################################
 
-readonly COMMAND_USAGE='Usage: bash vss_azure_bulk_event_stream_deactivate.sh  <Subscription_1> <Subscription_2> <Subscription_3>'
-readonly BASE_ARIA_SECURE_CLOUDS_FQDN="securestate.vmware.com"
-readonly BASE_CSP_FQDN="console.cloud.vmware.com"
-
+readonly COMMAND_USAGE='Usage: bash vss_aws_bulk_deactivate_event_stream.sh  <AccountID_1>{,<RootRoleName_1>} <AccountID_2>{,<RootRoleName_2>} where RootRoleName_1 is optional'
 readonly MAXIMUM_NUMBER_OF_ARGUMENTS=100
-readonly FAILED_PREFIX="failed_subs_"
-readonly RESOURCE_GROUP="cc_az_webhook_resource_group"
-readonly WEBHOOK_EVENT_STREAM_STATUS_URL="https://events.${BASE_ARIA_SECURE_CLOUDS_FQDN}/status"
-readonly CSP_AUTH_URL="https://${BASE_CSP_FQDN}/csp/gateway/am/api/auth/api-tokens/authorize"
+readonly AWS_DEFAULT_ROOT_ROLE="OrganizationAccountAccessRole"
+readonly VSS_TOPIC_NAME="cloudcoreo-events"
+readonly STACK=cloudcoreo-events
 
-declare -a SUBSCRIPTIONS_INFO_ARRAY
+declare -a ACCOUNTS_INFO_ARRAY
+declare -a UNSUCCESSFUL_ACCOUNTS
+declare -a ENABLED_REGIONS
+declare ROOT_ACCOUNT
 
 readonly _COLOR_GREEN=`tput setaf 2`
 readonly _COLOR_RED=`tput setaf 1`
 readonly _COLOR_YELLOW=`tput setaf 3`
 readonly _COLOR_MAGENTA=`tput setaf 5`
 readonly _COLOR_RESET=`tput sgr0`
-readonly I=${_COLOR_GREEN}INFO:${_COLOR_RESET}
-readonly E=${_COLOR_RED}ERROR:${_COLOR_RESET}
 
-function log() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
-}
 
 #######################################
 # Validates environment in which script is run.
 # Returns:
 #   0 if ok, non-zero on error.
 #######################################
-function validate_environment(){
-  # Checks whether az command exists - exit if not
-  if ! command -v az >/dev/null 2>&1
+validate_environment(){
+  # Checks whether aws command exists - exit if not
+  if ! command -v aws >/dev/null 2>&1
   then
-    echo "$E Please install AZ Command Line Interface: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli"
-    exit 1
-  fi
-
-  # Checks whether CSP_REFRESH_TOKEN environment variable exists - exit if not
-  if [ -z "${CSP_REFRESH_TOKEN}" ]; then
-    log  "$E Please set CSP_REFRESH_TOKEN environment variable: https://docs.vmware.com/en/CloudHealth-Secure-State/services/api-reference/GUID-getting-started.html"
+    log "${_COLOR_RED}ERROR:${_COLOR_RESET} Please install AWS Command Line Interface first: http://docs.aws.amazon.com/cli/latest/userguide/installing.html"
     exit 1
   fi
 }
+
 
 #######################################
 # Validates and extracts inputs of script
 # Arguments:
-#   Array of the following string entries: <Subscription_1> <Subscription_2> <Subscription_3>
+#   Array of the following string entries: <AccountID_1>,<RootRoleName_1>
 # Outputs:
-#   non-zero on error.
+#   0 if ok, non-zero on error.
 #######################################
-function extract_input_arguments() {
-  # Check if arguments are more than the limit
-  if [ "$#" -lt 1 ]; then
-    log "$E Number of input arguments is 0, please provide input arguments. $COMMAND_USAGE "
-    exit 1
-  fi
+extract_input_arguments() {
+  local index=0
 
   # Check if arguments are more than the limit
   if [ "$#" -gt $MAXIMUM_NUMBER_OF_ARGUMENTS ]; then
-    log "$E Number of input arguments exceeds the limit of ${MAXIMUM_NUMBER_OF_ARGUMENTS}"
+    log "${_COLOR_RED}ERROR:${_COLOR_RESET} Number of input arguments exceeds the limit of ${MAXIMUM_NUMBER_OF_ARGUMENTS}"
     exit 1
   fi
 
   # Extract arguments
-  for sub in "$@"; do
-    SUBSCRIPTIONS_INFO_ARRAY+=("$sub")
+  for tuple in "$@"; do
+    index=${index}+1
+
+    IFS=, read acc_id root_role <<< "$tuple"
+
+    if [ -z "${acc_id}" ]; then
+      log "${_COLOR_RED}ERROR:${_COLOR_RESET} on $index argument with value $var\nThe arguments <AccountID_1>,<RootRoleName_1> are required. $COMMAND_USAGE"
+      exit 1
+    fi
+
+    if [ -z "${root_role}" ]; then
+      root_role="${AWS_DEFAULT_ROOT_ROLE}"
+    fi
+
+    ACCOUNTS_INFO_ARRAY+=("$acc_id,$root_role")
+
   done
 }
 
 #######################################
-# Adds unsuccessful Subscriptions for individual examination
+# Assumes role for root account
 # Arguments:
-#   String:  "<Subscription_1>"
-#######################################
-function fail_subscriptions() {
-  log >> $FAILED_PREFIX"$1"
-}
-
-#######################################
-# Check for failed subscriptions and print
-#######################################
-function read_failed_subscriptions() {
-  failed_subs=$(ls | grep $FAILED_PREFIX)
-  if [ -n "$failed_subs" ]; then
-    log
-    log "${_COLOR_RED}********FAILED SUBSCRIPTIONS LIST********${_COLOR_RESET}"
-    for f in $failed_subs
-    do
-      rm $f
-      f_sub_id=${f//$FAILED_PREFIX/""}
-      log "${_COLOR_RED}$f_sub_id ${_COLOR_RESET}"
-    done
-  else
-    log "${_COLOR_GREEN}INFO: All subscriptions passed successfully.${_COLOR_RESET}"
-  fi
-}
-
-#######################################
-# Deactivate event stream for subscription:
-#   Delete resource group
-#   Send stream not ready hookup event to CHSS
-# Arguments:
-#   Subscription
+#   AccountId
+#   RootRoleName
 # Outputs:
-#   non-zero on error.
+#   Exports aws creds
 #######################################
-function vss_az_subs_deactivate() {
-  local sub_id=$1
-  log "$I Start deactivate event stream for $sub_id subscription."
+assume_role() {
+  use_aws_credentials_file
 
-  # Delete a resource group for the action group
-  az group delete --resource-group $RESOURCE_GROUP --yes --subscription "$sub_id" && l_ok=OK || l_ok=""
-  if [ -z "$l_ok" ]; then
-    log "$E Failed when trying to delete resource group $RESOURCE_GROUP for subscription $sub_id"
-    fail_subscriptions "$sub_id"
-    return 1
-  else
-    log "$I Successfully deleted resource group $RESOURCE_GROUP for subscription $sub_id"
-  fi
+  log  "${_COLOR_GREEN}INFO:${_COLOR_RESET} Getting credentials for account ${1} for role ${2}"
 
-  # Send stream removal success event.
-  local access_token=$(curl -s -X POST ${CSP_AUTH_URL} -d refresh_token="${CSP_REFRESH_TOKEN}" | jq -r .access_token)
-      if [ "$access_token" != "null" ]; then
-          log  "$I Successfully received bearer token"
-      else
-          log  "$E Failed to retrieve bearer token: refresh token is most likely incorrectly set"
-          fail_subscriptions "$sub_id"
-          return 1
-      fi
-
-  local stream_stop_event=$(curl -s -X POST ${WEBHOOK_EVENT_STREAM_STATUS_URL} -H "Authorization: Bearer ${access_token}" -d "{\"provider\": \"azure\", \"account_id\": \"${sub_id}\", \"active\": false }" )
-      if [ "$stream_stop_event" == "Accepted" ]; then
-          log  "$I Successfully sent stop stream hookup event to CHSS ${WEBHOOK_EVENT_STREAM_STATUS_URL} for subscription $sub_id"
-      else
-          log  "$E Failed to send stop stream hookup success event for subscription $sub_id $stream_stop_event"
-        fail_subscriptions "$sub_id"
-        return 1
-      fi
-
+  export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
+  $(aws sts assume-role \
+  --role-arn arn:aws:iam::$1:role/$2 \
+  --role-session-name session_in_acc_$1 \
+  --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+  --output text))
 }
 
 #######################################
-# Starts the process for event stream deactivated for each subs specified
+# Starts the process for Accounts Onboarding for each account specified
 # Globals:
-#   SUBSCRIPTIONS_INFO_ARRAY
+#   ACCOUNTS_INFO_ARRAY
 #######################################
-function vss_bulk_subs_deactivate() {
-  for subs in "${SUBSCRIPTIONS_INFO_ARRAY[@]}"; do
-    vss_az_subs_deactivate "$subs" &
+vss_deactivate_streams() {
+
+  for account in "${ACCOUNTS_INFO_ARRAY[@]}";  do
+    IFS=, read acc_id root_role <<< "$account"
+
+    # Skip assume role for root account
+    if [ "${acc_id}" != "${ROOT_ACCOUNT}" ]; then
+      assume_role "${acc_id}" "${root_role}"
+    fi
+
+    if { [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ] || [ -z "${AWS_SESSION_TOKEN}" ]; } && [ "${acc_id}" != "${ROOT_ACCOUNT}" ]; then
+      log "${_COLOR_MAGENTA}WARNING: ${_COLOR_RESET}Account $acc_id is skipped, due to error when fetching credentials"
+      fail_account "$account"
+    else
+      account_stream_deactivation "${acc_id}"
+      if [ ! $? -eq 0 ]; then
+        log "${_COLOR_RED}ERROR:${_COLOR_RESET} Account Event Deactivation process for $account failed."
+        fail_account "$account"
+      fi
+    fi
+
+    use_aws_credentials_file
+  done
+}
+
+#######################################
+# Onboards an account
+# Globals:
+# Arguments:
+#   AccountId
+#######################################
+account_stream_deactivation() {
+  local acc_id_param=$1
+
+  log  "${_COLOR_GREEN}INFO:${_COLOR_RESET} Starting account stream deactivation process for account ${acc_id_param} "
+
+  # Refresh Enabled Regions for account
+  if ! refresh_enabled_regions "${acc_id_param}"; then
+    log "${_COLOR_RED}ERROR:${_COLOR_RESET} Getting regions for ${acc_id_param} failed."
+    return 1
+  fi
+
+  for region in "${ENABLED_REGIONS[@]}";  do
+    delete_cf_stack "${acc_id_param}" "${region}" &
   done
   wait
 }
 
-function main() {
+#######################################
+# Installs CloudFormation Stack from Template
+# Globals:
+#   TEMPLATE_URL
+#   VERSION
+#   VSS_QUEUE_ARN
+#   VSS_TOPIC_NAME
+#   VSS_MONITORING_RULE
+#   VSS_TOPIC_ENCRYPTION_KEY_NAME
+# Arguments:
+#   AccountId
+#   RegionName
+#######################################
+delete_cf_stack(){
+  local acc_id_param=$1
+  local region_name=$2
+  local topic_arn="arn:aws:sns:${region_name}:${acc_id_param}:${VSS_TOPIC_NAME}"
+
+  list_topics=$(aws sns list-topics --output text --region "${region_name}")
+  has_topic=$(echo "${list_topics}" | grep ${topic_arn})
+  if [ -z "${has_topic}" ]; then
+    # Region is skipped as it doesn't have the required topic.
+    #log  "${_COLOR_GREEN}INFO:${_COLOR_RESET} Region ${region_name} is skipped as it doesn't have the topic ${topic_arn}"
+    return 0
+  fi
+
+  log  "${_COLOR_GREEN}INFO:${_COLOR_RESET} Starting deletion of cloudformation stack for account ${acc_id_param} in region ${region_name}"
+  topic_message=$(aws sns publish --topic-arn "${topic_arn}" --message UnsubscribeConfirmation --region "${region_name}")
+
+  # Deleting stack if exists
+  stack=$(aws cloudformation delete-stack --stack-name ${STACK} --region "${region_name}")
+
+  if [ $? -eq 0 ]; then
+    log  "${_COLOR_GREEN}INFO:${_COLOR_RESET} Successfully deleted cloudformation stack ${STACK} for account ${acc_id_param} in region ${region_name}"
+    return 0
+  else
+    log "${_COLOR_RED}ERROR:${_COLOR_RESET} Account Stack Deletion process for ${acc_id_param} failed."
+    return 1
+  fi
+}
+
+#######################################
+# Refresh List of Enabled Regions
+# Globals:
+#   ENABLED_REGIONS
+# Arguments:
+#   AccountId
+#######################################
+refresh_enabled_regions(){
+  local regions=( "af-south-1" "eu-north-1" "ap-south-1" "eu-west-3" "eu-west-2" "eu-south-1" "eu-west-1" "ap-northeast-3" "ap-northeast-2" "me-south-1" "ap-northeast-1" "sa-east-1" "ca-central-1" "ap-east-1" "ap-southeast-1" "ap-southeast-2" "eu-central-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
+  local acc_id_param=$1
+  log  "${_COLOR_GREEN}INFO:${_COLOR_RESET} Getting regions for account ${acc_id_param} "
+
+  enabled_regions=$(aws ec2 describe-regions --query "Regions[].{Name:RegionName}" --output text)
+
+  if [ -z "${enabled_regions}" ]; then
+    unset ENABLED_REGIONS
+    ENABLED_REGIONS=("${regions[@]}")
+  else
+    unset ENABLED_REGIONS
+    IFS=$'\n' read -r -d '' -a ENABLED_REGIONS <<< "$enabled_regions"
+  fi
+  echo "${ENABLED_REGIONS[@]}"
+}
+
+#######################################
+# Clears all aws creds so that the aws cli uses the ~/.aws/credentials file as source for creds
+#######################################
+use_aws_credentials_file() {
+  export AWS_ACCESS_KEY_ID=
+  export AWS_SECRET_ACCESS_KEY=
+  export AWS_SESSION_TOKEN=
+}
+
+#######################################
+# List all unsuccessful accounts
+# Globals:
+#   UNSUCCESSFUL_ACCOUNTS
+#######################################
+list_failed_accounts() {
+  if [ ${#UNSUCCESSFUL_ACCOUNTS[@]} -eq 0 ]; then
+    log "${_COLOR_GREEN}INFO:${_COLOR_RESET} All accounts passed successfully."
+  else
+    log "${_COLOR_RED}Failed Accounts: ${_COLOR_YELLOW}${UNSUCCESSFUL_ACCOUNTS[*]}${_COLOR_RESET}"
+  fi
+}
+
+
+#######################################
+# Adds unsuccessful accounts for individual examination
+# Arguments:
+#   String: "<AccountID_1>{,<RootRoleName_1>}"
+#######################################
+fail_account() {
+  UNSUCCESSFUL_ACCOUNTS+=("$1")
+}
+
+
+log() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+#######################################
+# Gets root account
+# Globals:
+#   ROOT_ACCOUNT
+#######################################
+function get_root_account() {
+  log  "${_COLOR_GREEN}INFO:${_COLOR_RESET} Getting root account id for session's aws credentials."
+
+  ROOT_ACCOUNT=$(aws sts get-caller-identity --output text --query "Account")
+  if [ $? -ne 0 ]; then
+    log  "${_COLOR_RED}ERROR:${_COLOR_RESET} Couldn't get root account."
+    exit 1
+  fi
+}
+
+main() {
+  log "${_COLOR_GREEN}INFO:${_COLOR_RESET} Running for $# accounts."
   validate_environment
-  log "$I Running deactivate azure event streams for $# subscriptions."
   extract_input_arguments "$@"
-  vss_bulk_subs_deactivate
-  read_failed_subscriptions
+  get_root_account
+  vss_deactivate_streams
+  list_failed_accounts
 }
 
 main "$@"
